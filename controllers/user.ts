13c723b18381f6e26075b30ca8a2a22a65759c60
vlzelensky <vzelensky.exceedteam@gmail.com>
2022-09-13 15:14:33 +0300

    initial commit

---
+++ controllers/user.ts
@@ -0,0 +1,61 @@
+//@ts-nocheck
+const User = require('../Models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async function (req, res) {
+  try {
+    const { name, userName, birthDate, email, password } = req.body;
+    const candidate = await User.findOne({ email });
+    if (candidate) {
+      return res.status(400).json({ message: 'User with this email already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = User({
+      name,
+      userName,
+      birthDate,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.status(201).json({ message: 'success' });
+  } catch (e) {
+    res.status(500).json({ message: 'Something went wrong, try again' });
+  }
+};
+
+exports.login = async function (req, res) {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid username / password pair' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid username / password pair' });
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    res.json({
+      email: user.email,
+      name: user.name,
+      birthDate: user.birthDate,
+      userName: user.userName,
+      token,
+      userId: user.id,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: 'Something went wrong, please try again' });
+  }
+};
---
+++ index.ts
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const user = require('./controllers/user.ts');
+
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/user/register', user.register);
+app.post('/user/login', user.login);
+
+mongoose.connect('mongodb://localhost:27017/', {
+  dbName: 'chat-db',
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(8080, () => {
+  console.log('server has started, PORT: 8080');
+});
